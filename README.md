abd0baa4e0b4a58116840720473e60bffef9d896
smcho <chos5@nku.edu>
2021-11-21 20:17:23 -0500

    Delete and modified.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Introduction
 
+What about you?
+Is it really good to say something together?
+
 Hello, this is to check the capabilities of Visual Studio Code.
--- js/main.js
+++ js/main.js
@@ -9,3 +9,5 @@ const you = [person, person]
 
 console.log(person.name)
 console.log(you.reduce((prev, cur) => prev + cur.age, 0)) 
+console.log(you.age)
+// Hello, Mr. Monkey, I'd like to say that we're in the same business. 
